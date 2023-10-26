7c65bb1e91744c80fd0810aead886bb9156e5a8e
joeyzhou1984 <joeyzhou1984@gmail.com>
2023-10-25 22:47:41 -0700

    add example of using replicate streaming api

--- anymodality/llms/replicate.py
+++ anymodality/llms/replicate.py
@@ -1,20 +1,51 @@
+from typing import Dict, Iterator
 import replicate
 from anymodality.llms.base import BaseLLM
 
+from enum import Enum
+
+
+class StreamingSupportType(Enum):
+    NON_STREAMING = 0
+    CAN_STREAMING = 1
+    ONLY_STREAMING = 2
+
+# TODO define a schema for model spec, including expected input/oputput
+KNOWN_MODELS = {
+    "mini_gpt4": {
+        "model_str": "daanelson/minigpt-4:b96a2f33cc8e4b0aa23eacfce731b9c41a7d9466d9ed4e167375587b54db9423",
+        "streaming_mode": StreamingSupportType.NON_STREAMING,
+        "input_schema": {},
+        "output_schema": {},
+    },
+    "llava": {
+        "model_str": "yorickvp/llava-13b:2facb4a474a0462c15041b78b1ad70952ea46b5ec6ad29583c0b29dbd4249591",
+        "streaming_mode": StreamingSupportType.CAN_STREAMING,
+        "input_schema": {},
+        "output_schema": {},
+    }
+}
 
 class ReplicateLLM(BaseLLM):
     def complete():
         pass
 
-    # @staticmethod
     def visual_question_answer(
         self,
         model: str,
         input: dict,
         stream: bool = False,
-    ):
-        output = replicate.run(
-            model,
-            input=input,
-        )
-        return output
+    ) -> Dict[str, str] | Iterator[str]:
+        if stream:
+            output: Iterator[str] = replicate.run(
+                model,
+                input=input,
+            )
+            return output
+        else:
+            output: Dict[str, str] = replicate.run(
+                model,
+                input=input,
+            )
+            return output
+
--- tests/test_task.py
+++ tests/test_task.py
@@ -1,12 +1,15 @@
+import pytest
 from anymodality import Task
 
 
+# TODO refactor to more generic unit tests
 class TestClassVQAReplicate:
     def test_dialog_replicate(self):
         task = Task("visual_question_answering")
         response = task(
             llm="replicate",
-            model="daanelson/minigpt-4:b96a2f33cc8e4b0aa23eacfce731b9c41a7d9466d9ed4e167375587b54db9423",
+            # model="daanelson/minigpt-4:b96a2f33cc8e4b0aa23eacfce731b9c41a7d9466d9ed4e167375587b54db9423",
+            model="yorickvp/llava-13b:2facb4a474a0462c15041b78b1ad70952ea46b5ec6ad29583c0b29dbd4249591",
             input={
                 "image": open("static/parking.jpg", "rb"),
                 "prompt": "It is Wednesday at 4 pm. Can I park at the spot right now? Tell me in 1 line.",
